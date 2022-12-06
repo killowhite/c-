5bbb0ef89dfcde85c39d3b0e18288c0c70dca50f
first project <ddamage 11@gmail.com>
2022-12-06 17:25:21 +0600

    второе дз

---
+++ c#/дз/дз 1 семинар/2 семинар/Program.cs
@@ -0,0 +1,33 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/* Console.Write("Введите число: ");
+int threeNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]); */
+
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+/* Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberTxt = Convert.ToString(anyNumber);
+if (anyNumberTxt.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberTxt[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+} */
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+/* Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+
+void CheckingDayOfWeek (int dayNum){
+
+
+  if (dayNum == 6 || dayNum == 7) {
+  Console.WriteLine("( выходной) -> да");
+  }
+  else 
+  {Console.WriteLine("(этот день не выходной) -> нет");}}
+CheckingDayOfWeek(dayNum);*/
+
