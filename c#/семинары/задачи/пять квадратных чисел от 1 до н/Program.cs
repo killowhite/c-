d71bf347ac1f18e283e2feb28d117e65c24dcc52
first project <ddamage 11@gmail.com>
2022-12-07 20:02:53 +0600

    cvadrat

---
+++ c#/семинары/задачи/пять квадратных чисел от 1 до н/Program.cs
@@ -0,0 +1,16 @@
+﻿// Ввод н вывод 5 квадратных чисел до н
+void Squares(int number)
+{
+    int counter = 1;
+    while(counter <= number)
+    {
+        Console.Write($"{Math.Pow(counter, 2)}");
+        if(counter != number) Console.Write(" , ");
+        counter++;
+    }
+}
+Console.Write("Напишите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("ряд квадратных чисел");
+Squares(num);
