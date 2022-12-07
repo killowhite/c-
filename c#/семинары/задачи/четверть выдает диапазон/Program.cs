b38e6a49b1cc5e8e40aebb5ffa3ed3a8d1f6fae1
first project <ddamage 11@gmail.com>
2022-12-07 17:49:28 +0600

    четверть на ввод диапозон на вывод

---
+++ c#/семинары/задачи/четверть выдает диапазон/Program.cs
@@ -0,0 +1,12 @@
+﻿// пользователь вводит четверть программа выдает диапозон возможных координат
+void Diapozon(int quadrant)
+{
+    if(quadrant == 1)Console.WriteLine("x > 0, y > 0");
+   else if(quadrant == 2)Console.WriteLine("x < 0, y > 0");
+   else if(quadrant == 3)Console.WriteLine("x < 0, y < 0");
+ else if(quadrant == 4)Console.WriteLine("x > 0, y < 0");
+    else Console.WriteLine("Это не четверть");
+}
+Console.WriteLine("Введите четверть");
+int quadrantPoint = Convert.ToInt32(Console.ReadLine());
+Diapozon(quadrantPoint);
