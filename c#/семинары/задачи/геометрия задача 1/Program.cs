c642c8302a5599cb2f742025c12a47293aeb08ad
First <ddamage11@gmail.com>
2022-12-07 06:49:37 +0600

    1 задача

---
+++ c#/семинары/задачи/геометрия задача 1/Program.cs
@@ -0,0 +1,16 @@
+﻿int FindQuardant(double x, double y)
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return 0;
+}
+Console.WriteLine("input x");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input y");
+double yA = Convert.ToDouble(Console.ReadLine());
+
+int quadNum = FindQuardant(xA, yA);
+
+Console.WriteLine($"The point A({xA}, {yA}) is DateOnly the {quadNum}");
