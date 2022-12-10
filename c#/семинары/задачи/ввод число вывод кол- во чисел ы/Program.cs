38c108e1260f7ac3a8030ba947cae89a4825647e
first project <ddamage 11@gmail.com>
2022-12-10 18:10:45 +0600

    1 zadacha

---
+++ c#/семинары/задачи/ввод число вывод кол- во чисел ы/Program.cs
@@ -0,0 +1,22 @@
+﻿// Поьзователь вводит число, программа выводит кол-во цифр в числе
+/*Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+string txt = Convert.ToString(num);
+*/
+
+int NumOfDigits(int num)
+{
+    int count = 0;
+    if(num == 0) count = 1;
+    while(num !=0)
+    {
+        count++;
+        num /=10;
+    } 
+    return count;
+}
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = NumOfDigits(number);
+Console.WriteLine($"Кол-во чисел в {number} {result}");
