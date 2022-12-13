83206db884e81060713664bbf30cdcd92a3b1510
first project <ddamage 11@gmail.com>
2022-12-13 16:35:16 +0600

    final

---
+++ c#/семинары/задачи/на ввод н вывод н факториал/Program.cs
@@ -0,0 +1,13 @@
+﻿// принимает н выдает н факториал
+int facktorial(int num){
+    int f = 1;
+    while(num>1){
+        f *=num;
+        num--;
+    }
+return f;
+}
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = facktorial(number);
+Console.WriteLine($"факториал числа = {result}");
