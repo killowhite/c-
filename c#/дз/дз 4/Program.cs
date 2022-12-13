a2a5a8603cced0a0ea338aab76d0e318019eff8b
first project <ddamage 11@gmail.com>
2022-12-13 22:09:20 +0600

    dz

---
+++ c#/дз/дз 4/Program.cs
@@ -0,0 +1,59 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+
+/* int Exponentiation(int numA, int numB)
+{
+  int result = 1;
+  for(int i=1; i <= numB; i++)
+  {
+    result = result * numA;
+  }
+
+    return result;
+}
+
+  Console.Write("Введите число A: ");
+  int numA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numB = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(numA, numB);
+  Console.WriteLine(exponentiation); */
+
+  // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+ /* Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int num)
+  {
+    
+    int counter = Convert.ToString(num).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = num - num % 10;
+      result = result + (num - advance);
+      num = num / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(num);
+Console.WriteLine("Сумма " + sumNumber);*/
+
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] Array(int size = 8)
+{
+    int[] Array = new int[8];
+}
+void ShowArray(int[]array){
+    for(i=0; i < array.Length; i++)
+    {
+        Console.WriteLine($"array[{i+1}] is {array[i]}");
+    }
+}
+int myArray =  Array();
+ShowArray(myArray);
