52f30bc0023634912841fb2044641d78b41febe6
first project <ddamage 11@gmail.com>
2022-12-13 16:20:43 +0600

    final

---
+++ c#/семинары/задачи/принимает число и выдает сумму чисел от 1 до н/Program.cs
@@ -0,0 +1,16 @@
+﻿// Принимает н выдает сумму от 1 до н
+
+int sum(int num)
+{
+    int sum = 1;
+while(num > 1){
+    sum +=num;
+    num--;
+}
+return sum;
+}
+Console.WriteLine("ввеите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = sum(number);
+Console.WriteLine($"сумма от 1 до н = {result}");
