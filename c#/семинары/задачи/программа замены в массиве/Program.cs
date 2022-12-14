8fc28ed17f473279fcbc7d3d1536f7b0a27da3b2
first project <ddamage 11@gmail.com>
2022-12-14 20:04:00 +0600

    final

---
+++ c#/семинары/задачи/программа замены в массиве/Program.cs
@@ -0,0 +1,40 @@
+﻿// программа превращает отрицательные числа в положительные и наоборот
+int[] GreateArray(int size, int minValue, int maxValue)
+{
+    int[] array= new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+void ShowArray(int[]array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"array[{i+1}] is {array[i]}");
+    }
+}
+
+int[] revers(int[]array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        
+        array[i] *= -1;
+        
+    }
+    return array;
+}
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введитее минимальное значение");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = GreateArray(size, min, max);
+ShowArray(myArray);
+int[] result = revers(myArray);
+ShowArray(result);
