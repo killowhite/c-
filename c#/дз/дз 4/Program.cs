b58a64e23095e88246d43d2c39be7d5772b1e084
First <ddamage11@gmail.com>
2022-12-15 14:56:32 +0600

    3zad

--- c#/дз/дз 4/Program.cs
+++ c#/дз/дз 4/Program.cs
@@ -45,3 +45,21 @@ Console.WriteLine("Сумма " + sumNumber);*/
 
 
 // Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] GreateArray(int size,int minValue, int maxValue)
+{
+  int[] array = new int[size];
+  for(int i = 0; i < size; i++)
+  {
+    array[i] = new Random().Next(minValue, maxValue +1);
+  }
+  return array;
+}
+
+void ShowArray(int[]array
+{
+  for(int i = 0; i < array.Length; i++)
+  {
+Console.WriteLine($"array[{i +1}] is {array[i]}");
+  }
+})
