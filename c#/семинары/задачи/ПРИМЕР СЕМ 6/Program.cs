6951f1f1c18f987aa7815c415170d805aaa57ca3
First <ddamage11@gmail.com>
2022-12-20 14:32:10 +0600

    final

---
+++ c#/семинары/задачи/ПРИМЕР СЕМ 6/Program.cs
@@ -0,0 +1,40 @@
+﻿//Разворот массива
+int[] GreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+} 
+
+void ShowArray(int[]array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"array[{i+1}] is {array[i]}");
+    }
+}
+
+void ReversArray(int[]array)
+{
+    for(int i = 0, j = array.Length - 1; i < j; i++, j++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введитее минимальное значение");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = GreateRandomArray(size, min, max);
+ShowArray(myArray);
+ReversArray(myArray);
+ShowArray(myArray);
